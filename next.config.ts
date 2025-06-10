75ee181b8164b8db3cb77537d10240176729b861
naki <ekagra7865@gmail.com>
2025-06-10 21:38:30 +0000

    When i try to test the app it gives popup closed by user
    Use this page

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,20 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
